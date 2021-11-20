50fedb61aec20ea0ac1604366e7c34024d52a068
Scott Popplewell <scottdpopplewell@protonmail.com>
2021-11-19 18:05:20 -0600

    chore: removes local recipes

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,13 +25,6 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/content/blog`,
-        name: `blog`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
